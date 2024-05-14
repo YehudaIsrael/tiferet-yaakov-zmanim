1833a0e2dbcd6dd6639a3f8aab3dd4d0ae25d0d2
Yehuda Israel <yehuda.israel@ironvest.com>
2024-05-14 19:04:33 +0300

    fix bug in omer and after shabbat

--- src/hooks/useCalendar.ts
+++ src/hooks/useCalendar.ts
@@ -92,26 +92,27 @@ export const useCalendar = () => {
     const nightfall = dayjs(todayRow['שקיעה קטגוריה'], 'HH:mm:ss')
       .add(12, 'hours')
       .add(20, 'minutes');
+
     const now = dayjs();
     const tomorrow = now.add(1, 'day').startOf('day');
+    const tomorrowFormatted = tomorrow.format('D.M.YYYY');
+    const nextRow = calendarData.current.find(day => day['תאריך לועזי'] === tomorrowFormatted);
 
     if (todayRow['צאת הכוכבים ר"ת 72 שוות קטגוריה']) {
-      const afterShabbat = dayjs(todayRow['צאת הכוכבים ר"ת 72 שוות קטגוריה'], 'HH:mm:ss')
+      const afterShabbat = dayjs(todayRow['צאת הכוכבים ר"ת 72 שוות קטגוריה'], 'HH:mm')
         .add(12, 'hours')
         .add(10, 'minutes');
-      if (todayRow['ספירת העומר']) {
-        todayRow['ספירת העומר'] = +todayRow['ספירת העומר'] + 1;
-        return todayRow;
-      }
       if (now < afterShabbat) {
+        if (todayRow['ספירת העומר'] && nightfall < afterShabbat) {
+          todayRow['ספירת העומר'] = nextRow?.['ספירת העומר'] || '';
+          return todayRow;
+        }
         return;
       }
     }
 
     if (now > nightfall && now < tomorrow) {
-      const tomorrowFormatted = tomorrow.format('D.M.YYYY');
-      const nextRow = calendarData.current.find(day => day['תאריך לועזי'] === tomorrowFormatted);
-      if (!nextRow || nextRow['תאריך לועזי'] === todayRow['תאריך לועזי']) return;
+      if (nextRow?.['תאריך לועזי'] === todayRow['תאריך לועזי']) return;
       return nextRow;
     }
     return;
